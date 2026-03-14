aaa0d8e35ad0461472921f0260ecd7555f4a11db
djamn <david@jamnig.net>
2026-03-14 23:14:56 +0100

    restructured code

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/MainActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/MainActivity.kt
@@ -11,19 +11,19 @@ import androidx.activity.enableEdgeToEdge
 import com.example.myapplication.R
 
 class MainActivity : ComponentActivity(), Callbacks {
-    lateinit var mystomp:MyStomp
-    lateinit var  response:TextView
+    lateinit var mystomp: MyStomp
+    lateinit var response: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
-        mystomp=MyStomp(this)
+        mystomp = MyStomp(this)
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fragment_fullscreen)
 
         findViewById<Button>(R.id.connectbtn).setOnClickListener { mystomp.connect() }
-        findViewById<Button>(R.id.hellobtn).setOnClickListener{mystomp.sendHello()}
-        findViewById<Button>(R.id.jsonbtn).setOnClickListener{mystomp.sendJson()}
-        response=findViewById(R.id.response_view)
+        findViewById<Button>(R.id.hellobtn).setOnClickListener { mystomp.sendHello() }
+        findViewById<Button>(R.id.jsonbtn).setOnClickListener { mystomp.sendJson() }
+        response = findViewById(R.id.response_view)
 
     }
 
