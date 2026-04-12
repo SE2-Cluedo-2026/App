ac644bcc4e6266b68e73483220fed87d54969bd7
Alsamman Baian <baian.alsamman@htl-klu.at>
2026-04-12 18:53:53 +0200

    Add Card class

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/model/Card.kt
@@ -0,0 +1,12 @@
+package at.aau.serg.websocketbrokerdemo.model
+
+data class Card (
+
+    val cardId: String,
+    val name: String
+    /*
+    toDo :
+    1. Karten Logik , welche Karte habe ich mit bekommen von Server
+
+     */
+    )
