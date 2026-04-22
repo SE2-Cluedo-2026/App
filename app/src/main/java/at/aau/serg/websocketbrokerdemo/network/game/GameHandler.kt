6a82b895e9a996efc88350ddcb3131d8c720fb35
LauroSchoendorfer <laurosc@edu.aau.at>
2026-04-22 21:56:26 +0200

    added GameHandler and LobbyHandler to have handle different messages from the Server

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/network/game/GameHandler.kt
@@ -0,0 +1,9 @@
+package at.aau.serg.websocketbrokerdemo.network.game
+
+class GameHandler {
+    companion object {
+        fun handle(msg: String) {
+            // TODO: Implement message Handling for GameMessage
+        }
+    }
+}
