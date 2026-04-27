4ba5f9f218ad32044103e69cd01bbb6c0f14e8be
Alsamman Baian <baian.alsamman@htl-klu.at>
2026-04-27 21:44:05 +0200

    Added GameMassageType

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/messaging/dtos/GameMessageType.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/messaging/dtos/GameMessageType.kt
@@ -1,5 +1,13 @@
 package at.aau.serg.websocketbrokerdemo.messaging.dtos
 
 enum class GameMessageType {
+    ROLL_DICE,
+    END_TURN,
+    MOVE,
+    ENTER_ROOM,
+    TAKE_HIDDEN_WAY,
+    MAKE_ACCUSATION,
+    MAKE_SUGGESTION
+
 
 }
