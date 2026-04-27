d267ea77870f85484c49496daa8b9bd42acee5a9
Vanesa Markovic <deine.uni.email@aau.at>
2026-04-27 12:57:48 +0200

    add GameActivity with board screen

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/GameActivity.kt
@@ -0,0 +1,12 @@
+package at.aau.serg.websocketbrokerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.myapplication.R
+
+class GameActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
