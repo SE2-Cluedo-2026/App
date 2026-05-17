f1ea76029773a4b8762f76c4826eb9283294fb68
Vanesa Markovic <deine.uni.email@aau.at>
2026-05-17 19:21:10 +0200

    feat(rejoinGame): reworked rejoin

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/messaging/dtos/LobbyMessageType.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/messaging/dtos/LobbyMessageType.kt
@@ -3,6 +3,7 @@ package at.aau.serg.websocketbrokerdemo.messaging.dtos
 enum class LobbyMessageType {
     NEW_PLAYER_JOINED,
     PLAYER_REJOINED,
+    PLAYER_REJOINED_RUNNING,
     GAME_FULL,
     PLAYER_REMOVED,
     SET_CHARACTER_TYPE_AND_STATUS_READY,
