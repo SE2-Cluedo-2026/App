1330a53a5e70fb7428019af056d1b0791e44c42a
mucica33 <kocmut.tea@gmail.com>
2026-06-07 11:10:28 +0200

    add onConnectionFailed and onConnectionLost to interface

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/Callbacks.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/Callbacks.kt
@@ -3,4 +3,6 @@ package at.aau.serg.websocketbrokerdemo
 interface Callbacks {
     fun onResponse(res: String);
     fun onConnected();
+    fun onConnectionFailed(reason: String)
+    fun onConnectionLost(reason: String)
 }
